1d9ae907f5835b29eb2010a3ce3576cee7fa7634
pedsm <peumendonca@gmail.com>
2018-04-28 14:10:33 +0100

    Organised files

--- scripts/merge.js
+++ scripts/merge.js
@@ -2,7 +2,7 @@ const nodes = require('./file.json')
 const links = require('./relationships.json')
 const fs = require('fs')
 
-fs.writeFileSync('data.json', JSON.stringify(
+fs.writeFileSync('../data.json', JSON.stringify(
     {
         nodes,
         links
